1231dbd65f0839e7d3e1dbce40e7a38dfbf47ab0
TopDevSteven <cheerman00526@gmail.com>
2023-08-19 08:12:59 -0700

    create_test

---
+++ app.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI, Request, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/")
+def read_root():
+    return "Hello"
+
+
+@app.get("/hello/")
+def hello():
+    return "Hello World"
